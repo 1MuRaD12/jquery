e174c390862f69190b332553127dbac2f174fbf8
1MuRaD12 <tu71tfsws@code.edu.az>
2022-05-16 10:39:05 +0300

    first commit

---
+++ assets/js/s.js
@@ -0,0 +1,55 @@
+$(".quesion .button1").click(function () {
+    $(this).css("color", "blue")
+    $(".answer .titlee1").toggle(1000);
+});
+$(".titl .h51").click(function () {
+
+    $(".p1").toggle(1000);
+})
+$(".titl .h52").click(function () {
+    $(".p2").toggle(1000);
+})
+$(".titl .h53").click(function () {
+    $(".p3").toggle(1000);
+})
+$(".titl .h54").click(function () {
+    $(".p4").toggle(1000);
+})
+
+$(".quesion .button2").click(function () {
+    $(this).css("color", "blue")
+    $(".answer .titlee2").toggle(1000);
+
+});
+$(".titl1 .h51").click(function () {
+
+    $(".p1").toggle(1000);
+})
+$(".titl1 .h52").click(function () {
+    $(".p2").toggle(1000);
+})
+$(".titl1 .h53").click(function () {
+    $(".p3").toggle(1000);
+})
+$(".titl1 .h54").click(function () {
+    $(".p4").toggle(1000);
+})
+
+$(".quesion .button3").click(function () {
+    $(this).css("color", "blue")
+    $(".answer .titlee3").toggle(1000);
+
+});
+$(".titl2 .h51").click(function () {
+
+    $(".p1").toggle(1000);
+})
+$(".titl2 .h52").click(function () {
+    $(".p2").toggle(1000);
+})
+$(".titl2 .h53").click(function () {
+    $(".p3").toggle(1000);
+})
+$(".titl2 .h54").click(function () {
+    $(".p4").toggle(1000);
+})
